0b3d119432c54b07aeeb722c705ad6c41fd21ed0
Sanjay <sanjaygouri@users.noreply.github.com>
2015-10-03 15:58:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -87,40 +87,6 @@ con.end(function(err) {
 });
 
 
-
-
-passport.use('local-login', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
-        usernameField : 'name',
-        passwordField : 's',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
-    function(req, email, password, done) { // callback with email and password from our form
-
-         connection.query("SELECT * FROM `fuck` WHERE `name` = '" + email + "'",function(err,rows){
-			if (err)
-                return done(err);
-			 if (!rows.length) {
-                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-            } 
-			
-			// if the user is found but the password is wrong
-            if (!( rows[0].password == password))
-                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-			
-            // all is well, return successful user
-            return done(null, rows[0]);			
-		
-		});
-		
-
-
-    }));
-
-
-
-
-
 app.get('/about',function(req,res){
 res.render('about');
 
